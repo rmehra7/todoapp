5a43abee269c65f282a8ad524fc4d42d08786af1
Rahul Mehra <rmehra9292@gmail.com>
2022-08-11 14:35:52 +0530

    fixed importes

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from server.app_server import server
 import sys, os
 from flask import render_template
 
 # Need to import all resources
-from api.rest_endpoint import server
-from api.web_endpoint import server
+from server.app_server import server
+from api.rest_endpoint import server as rest_api_server
+from api.web_endpoint import server as web_api_server
 
 if __name__ == '__main__':
     server.run()
